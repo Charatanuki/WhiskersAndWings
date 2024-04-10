787df2bff6c304d68bf6cdeaf6297c9848583a97
Lynn <jacquelinehuynh@outlook.fr>
2024-04-10 21:39:07 +0200

    add collision trap up

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -99,10 +99,15 @@ class Tilemap:
 
     def death_rects_around(self, pos):
         rects = []
-        for tile in self.tiles_around(pos):
+        for tile in self.tiles_around(pos): #collision with traps
             if tile['type'] in DEATH_TILES:
-                rects.append(
-                    pygame.Rect(tile['pos'][0] * self.tile_size, (tile['pos'][1] * (self.tile_size)) + self.tile_size // 2, self.tile_size,
+                if tile['variant'] == 0 or tile['variant'] == 4: #collision traps down
+                    rects.append(
+                        pygame.Rect(tile['pos'][0] * self.tile_size, (tile['pos'][1] * (self.tile_size)) + self.tile_size // 2, self.tile_size,
+                                self.tile_size // 2))
+                elif tile['variant'] == 2 or tile['variant'] == 6:#collision traps up
+                    rects.append(
+                        pygame.Rect(tile['pos'][0] * self.tile_size, (tile['pos'][1] * (self.tile_size)), self.tile_size,
                                 self.tile_size // 2))
         return rects
 
